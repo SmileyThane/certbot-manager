324ad8ace3b60e42a9ca95a989e7ffde32530995
Hleb Prakhnitski <thane.crios@gmail.com>
2022-07-02 12:24:19 +0400

    added main runner

---
+++ run.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+source variables.conf
+
+if hash certbot 2>/dev/null; then
+    echo certbot --version
+else
+   ./install_certbot.sh
+fi
+
+now="$(date '+%d.%m.%Y')"
+
+while read domain
+do
+exec > "logs/$domain-output-$now.txt" 2>&1
+    sudo certbot certonly
+    --authenticator $authenticator
+    --installer $server_app
+    --post-hook $post_hook
+    --force-renewal
+    -d $domain
+#   --preferred-chain "ISRG Root X1"
+done < $domains
+
+
